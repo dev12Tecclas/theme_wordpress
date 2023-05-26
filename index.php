9a6da1b530a7e231cc8bfb372fc98320f0821179
dev12Tecclas <129604075+dev12Tecclas@users.noreply.github.com>
2023-05-26 16:56:32 -0500

    asdsa

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?php wp_title(); ?></title>
     <?php wp_head(); ?>
 </head>
-<body>
-    <div id="root"></div>
+<body <?php body_class(); ?>>
+    <div id="root">
+        <!-- Aquí puedes agregar el contenido de tu tema -->
+        <?php
+            // Ejemplo: Mostrar el título del sitio
+            echo "<h1>" . get_bloginfo('name') . "</h1>";
+        ?>
+    </div>
+    <?php wp_footer(); ?>
 </body>
 </html>
